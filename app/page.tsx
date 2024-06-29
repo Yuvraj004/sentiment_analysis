7d06a59cf2cdbe905fe7be2b99b72c74bd0c91cf
Yuvraj004 <yuvrajchat@gmail.com>
2024-06-29 15:53:50 +0530

    color & emoji adding

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import axios from "axios";
 import { ChangeEvent,useEffect,useState } from "react";
+import { emotionConfig } from "./config";
 
 export default function Home() {
   //state variables
+  const defaultColor = "#cccccc";
   const [rows, setRows] = useState(2);
   const [input, setinput] = useState("");
   const [output, setOutput] = useState<{ label:string ; score:number}[]>();
   const [loading, setLoading] = useState(false);
+  const [color, setcolor] = useState(defaultColor); 
+  const [tagsVisible, setTagsVisible] = useState(false) ;
 
   useEffect(() => {
 
@@ -17,6 +21,20 @@ export default function Home() {
   
     return () => clearTimeout(inputTimeout);
   }, [input])//whenever input changes use effect will run
+  
+  useEffect(() => {
+    //
+    handleColor();
+  
+  }, [output])
+
+  function handleColor(){
+    if(output && output.length > 0){
+      const ckey = output[0].label;
+      const chex = (emotionConfig as any)[ckey].colorHex;
+      setcolor(chex);
+    }
+  }
   
   async function runPredictions() {
     
@@ -40,7 +58,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main style={{backgroundColor:color+"aa"}} className="transition-all delay-500 flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="lg:text-4xl text-2xl font-mono font-semibold tracking-tight">🖌🎨Paint my mood🎨🖌</h1>
       <div className="border border-black p-4 rounded-lg">
         <textarea 
@@ -56,7 +74,7 @@ export default function Home() {
       <div className="flex flex-wrap items-center justify-center gap-2">
         {output?.map(({label,score}) =>{
           
-          return  <span key={label} className="cursor-pointer bg-indigo-100 text-indigo-800 text-lg px-4 py-1 rounded-full border border-indigo-400">{label}</span>
+          return  <span key={label} className="cursor-pointer bg-indigo-100 text-indigo-800 text-lg px-4 py-1 rounded-full border border-indigo-400">{label} {(emotionConfig as any)[label].emoji}</span>
         })}
       </div>
     </main>
